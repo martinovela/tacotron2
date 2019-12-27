d1befad400e69c3128a24ad54d8e175916ca6cdb
martinovela <martinovela507@gmail.com>
2019-12-27 14:13:27 +0700

    change attention -> multihead attention"

--- model.py
+++ model.py
@@ -26,6 +26,60 @@ class LocationLayer(nn.Module):
         return processed_attention
 
 
+class MultiHeadAttention(nn.MultiheadAttention):
+    """
+    Scaled Dot-Product Attention
+    """
+
+    def __init__(self, input_size=512, output_size=512, num_heads=8, dropout=0, causal=False, bias=True, add_bias_kv=False, add_zero_attn=False, batch_first=True, groups=None, weight_norm=None):
+        super(MultiHeadAttention, self).__init__(input_size, num_heads, dropout=dropout,
+                                                 bias=bias, add_bias_kv=add_bias_kv, add_zero_attn=add_zero_attn)
+        assert(input_size % num_heads == 0)
+        assert(input_size == output_size)
+        self.causal = causal
+        self.batch_first = batch_first
+
+    def set_mask_q(self, masked_tq):
+        self.mask_q = masked_tq
+
+    def set_mask_k(self, masked_tk):
+        # applies a mask of b x tk length
+        self.mask_k = masked_tk
+
+    def forward(self, query, key, value, need_weights=False, static_kv=False):
+        key_padding_mask = attn_mask = None
+        time_dim = 1 if self.batch_first else 0
+        t_q = query.size(time_dim)
+        t_k = key.size(time_dim)
+        with torch.no_grad():
+            if self.causal and t_q > 1:
+                attn_mask = torch.full((t_q, t_k), float('-inf'),
+                                       device=query.device, dtype=query.dtype).triu_(1)
+            key_padding_mask = self.mask_k
+
+        if self.batch_first:
+            qkv_same = query.data_ptr() == key.data_ptr() == value.data_ptr()
+            kv_same = key.data_ptr() == value.data_ptr()
+            key = key.transpose(0, 1)
+            if kv_same:
+                value = key
+            else:
+                value = value.transpose(0, 1)
+            if qkv_same:
+                query = key
+            else:
+                query = query.transpose(0, 1)
+        elif key_padding_mask is not None:
+            key_padding_mask.t()
+
+
+        attn_output, attn_output_weights = super(
+            MultiHeadAttention, self).forward(query, key, value, key_padding_mask=key_padding_mask, attn_mask=attn_mask, need_weights=need_weights)
+        if self.batch_first:
+            attn_output = attn_output.transpose(0, 1)
+        return attn_output, attn_output_weights
+
+
 class Attention(nn.Module):
     def __init__(self, attention_rnn_dim, embedding_dim, attention_dim,
                  attention_location_n_filters, attention_location_kernel_size):
@@ -223,7 +277,12 @@ class Decoder(nn.Module):
             hparams.prenet_dim + hparams.encoder_embedding_dim,
             hparams.attention_rnn_dim)
 
-        self.attention_layer = Attention(
+        # self.attention_layer = Attention(
+        #     hparams.attention_rnn_dim, hparams.encoder_embedding_dim,
+        #     hparams.attention_dim, hparams.attention_location_n_filters,
+        #     hparams.attention_location_kernel_size)
+
+        self.attention_layer = MultiHeadAttention(
             hparams.attention_rnn_dim, hparams.encoder_embedding_dim,
             hparams.attention_dim, hparams.attention_location_n_filters,
             hparams.attention_location_kernel_size)
@@ -285,7 +344,8 @@ class Decoder(nn.Module):
             B, self.encoder_embedding_dim).zero_())
 
         self.memory = memory
-        self.processed_memory = self.attention_layer.memory_layer(memory)
+        # self.processed_memory = self.attention_layer.memory_layer(memory)
+        # self.processed_memory = memory
         self.mask = mask
 
     def parse_decoder_inputs(self, decoder_inputs):
@@ -355,12 +415,15 @@ class Decoder(nn.Module):
         self.attention_hidden = F.dropout(
             self.attention_hidden, self.p_attention_dropout, self.training)
 
-        attention_weights_cat = torch.cat(
-            (self.attention_weights.unsqueeze(1),
-             self.attention_weights_cum.unsqueeze(1)), dim=1)
+        # attention_weights_cat = torch.cat(
+        #     (self.attention_weights.unsqueeze(1),
+        #      self.attention_weights_cum.unsqueeze(1)), dim=1)
+        # self.attention_context, self.attention_weights = self.attention_layer(
+        #     self.attention_hidden, self.memory, self.processed_memory,
+        #     attention_weights_cat, self.mask)
+
         self.attention_context, self.attention_weights = self.attention_layer(
-            self.attention_hidden, self.memory, self.processed_memory,
-            attention_weights_cat, self.mask)
+            self.attention_hidden, self.memory, self.memory)
 
         self.attention_weights_cum += self.attention_weights
         decoder_input = torch.cat(
